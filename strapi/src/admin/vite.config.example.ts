1196827c005bb4b91aab39bb3e2109592994bd65
Ritesh2k24 <162950856+Ritesh2k24@users.noreply.github.com>
2025-04-27 10:34:33 +0530

    vite.config.example.ts
    updated

--- strapi/src/admin/vite.config.example.ts
+++ strapi/src/admin/vite.config.example.ts
@@ -1,3 +1,17 @@
+//import { mergeConfig, type UserConfig } from 'vite';
+
+//export default (config: UserConfig) => {
+// Important: always return the modified config
+//return mergeConfig(config, {
+//resolve: {
+//  alias: {
+//  '@': '/src',
+//  },
+//  },
+//  });
+//};
+
+
 import { mergeConfig, type UserConfig } from 'vite';
 
 export default (config: UserConfig) => {
@@ -7,6 +21,12 @@ export default (config: UserConfig) => {
       alias: {
         '@': '/src',
       },
+    },
+    server: {
+      host: true, // Listen on all addresses
+      allowedHosts: [
+        'strapi-alb-1913271914.us-east-1.elb.amazonaws.com',
+      ],
     },
   });
 };
