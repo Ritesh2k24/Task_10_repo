24844c721cc6d88c5d120017b48d7f09a8d74905
Ritesh2k24 <pawar.ritesh2018@gmail.com>
2025-04-29 09:25:57 +0000

    edited .tf files

--- Terraform_task10/vpc.tf
+++ Terraform_task10/vpc.tf
@@ -81,6 +81,15 @@ resource "aws_security_group" "strapi_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  description = "Allow HTTPS"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
   egress {
     from_port   = 0
     to_port     = 0
